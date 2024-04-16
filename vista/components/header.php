33b97c9a14311f8d154cdb428e64ab6defd7af4a
MatiasSalazar1029 <matiassalazar1029@gmail.com>
2024-04-16 22:09:29 +0200

    Añadiendo login e index

---
+++ vista/components/header.php
@@ -0,0 +1,21 @@
+<header>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="../../index.html">IMFCA CONTACTO</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+ 
+          </ul>
+          <div class="d-flex m-4" role="search">
+              <a class="nav-link active" aria-current="page" href="">Ingresar</a>
+          </div>
+          <div class="d-flex m-4" role="search">
+            <a class="nav-link active" aria-current="page" href="#">Registro</a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </header>
--- vista/login/login.php
+++ vista/login/login.php
@@ -8,8 +8,68 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link rel="stylesheet" href="./login.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <script src="https://kit.fontawesome.com/73c70fe811.js" crossorigin="anonymous"></script>
   </head>
   <body>
-    
-  </body>
-  </html>
+  <div class="contenedor">
+
+    <?php
+      @include '../components/header.php'
+    ?>
+
+    <div class="form-container">
+      
+      <form action="" method="post" class="formulario" id="formulario">
+          <h3 class="titulo-Formulario">INICIAR SESION</h3>
+          <?php
+              /* if(isset($error)){
+                  foreach($error as $error){
+                      echo '<span class="error-msg">'.$error.'</span>';
+                  };
+              }else if($errorr){
+                  foreach($errorr as $errorr){
+                      echo '<span class="error-msg">'.$errorr.'</span>';
+                  };
+              }; */
+          ?>
+      
+    <!-- Grupo: Correo Electronico -->
+        <div class="formulario__grupo 1" id="grupo__correo">
+          <div class="formulario__grupo-input">
+            <input type="text" class="formulario__input" name="email" id="correo" placeholder=" " value="<?php if(isset($email)) echo $email?>">
+            <label for="email" class="formulario__label">Correo Electrónico</label>
+            <i class="formulario__validacion-estado fas fa-times-circle"></i>
+            <i class="fa-solid fa-user"></i>
+      <!--                      <i class="fa-regular fa-envelope fa-xl iconos"></i>
+      -->	</div>
+                  
+          <p class="formulario__input-error">El correo solo puede contener letras, numeros, puntos, guiones y guion bajo. <b><i>ejemplo@correo.com</i></b></p>
+        </div>
+          
+    <!-- Grupo: Contraseña -->
+        <div class="formulario__grupo 2" id="grupo__password">
+            <div class="formulario__grupo-input">
+              <input type="password" class="formulario__input" name="password" id="password" placeholder=" ">
+              <label for="password" class="formulario__label">Contraseña</label>
+              <i class="formulario__validacion-estado fas fa-times-circle circulo-error"></i>
+              <i class="fa-solid fa-key"></i>
+
+                        <!-- <i class="fa-regular fa-envelope fa-xl iconos"></i> -->
+                                   
+              <p class="formulario__input-error">La contraseña tiene que ser de 3 a 12 dígitos.</p>
+            </div>
+            <div class="formulario__grupo formulario__grupo-btn-enviar">
+              <button type="submit" class="formulario__btn" name="submit" id="enviar" >Ingresar</button>
+            </div>
+        </div>
+      </form>
+    </div>      
+  </div>
+  <?php
+      @include '../components/footer.php'
+    ?>
+</body>
+</html>
