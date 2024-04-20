c030eb7f304be6b6e9fd91b1f3f9f30384ddf68c
MatiasSalazar1029 <matiassalazar1029@gmail.com>
2024-04-19 23:16:23 -0500

    inclusion de mensaje para interfaz principal

--- vista/components/footer.php
+++ vista/components/footer.php
@@ -1,20 +1,9 @@
-<div class="col-lg-4">
-          <h3>Enlaces</h3>
-          <ul>
-            <li><a href="#">Inicio</a></li>
-            <li><a href="#">Acerca de</a></li>
-            <li><a href="#">Contacto</a></li>
-          </ul>
-        </div>
+<footer>
+    <div class="container ">
+      <div class="row">
         <div class="col-lg-4">
-          <h3>Contacto</h3>
-          <p>Correo electrónico: info@example.com</p>
-          <p>Teléfono: 123-456-7890</p>
-        </div>
-        <div class="col-lg-4">
-          <h3>Derechos de autor</h3>
-          <p>(c) 2023 Mi Sitio Web. Todos los derechos reservados.</p>
-        </div>
+          <span class="copyright"> Sistema de evaluación de   &copy; <a   href="https://imfcacontacto.com/" target="_blank">IMFCA CONTACTO</a> 2024</span>
       </div>
+    </div>
     </div>
   </footer>
