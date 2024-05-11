16c7c1fff9acaad2ec376d89ff6c8028a426080a
MatiasSalazar1029 <matiassalazar1029@gmail.com>
2024-05-10 21:49:01 -0500

    lista despleable en el boton de examen en vista backoffice

---
+++ vista/dashboard/backoffice/crud/backofficeEditarExamen.php
@@ -0,0 +1,85 @@
+<?php
+session_start();
+@include '../../../modelo/conexion.php';
+
+if(!isset($_SESSION['backoffice_name'])){
+   header('location:../../login/login.php');
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Backoffice</title>
+  
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="../backoffice.css">
+    <link rel="stylesheet" href="../css/components/header.css">
+  <link rel="stylesheet" href="../../../css/components/header.css">
+  <link rel="stylesheet" href="../../css/components/footer.css">
+  <link rel="stylesheet" href="css/header.css" >
+    <link rel="stylesheet" href="./login.css">
+    <script src="https://kit.fontawesome.com/73c70fe811.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+<?php @include '../../../../vista/components/cerrarSesion.php'?>
+
+ 
+  
+   <div class="container">
+   <h1>Hola soy backoffice <?php echo $_SESSION['backoffice_name'] ?></h1>
+ <a href="../crud/backofficeCrearExamen.php"><button type="button" class="btn btn-primary">Crear examen</button></a>
+ <button type="button" class="btn btn-secondary">Ver resultados</button>
+ <button type="button" class="btn btn-success">Imprimir resultados</button>
+
+ <div class="container-table">
+    <table class="table table-bordered">
+    <thead>
+      <tr>
+        <th scope="col">ID Examen</th>
+        <th scope="col">Nombre</th>
+        <th scope="col">Fecha</th>
+        <th scope="col">Creado por</th>
+        <th scope="col">N° preguntas</th>
+        <th scope="col"></th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td scope="row">001</td>
+        <td>Prueba01</td>
+        <td>29/04/2024</td>
+        <td>Jose</td>
+        <td>5</td>
+        <td><button class="btn-delete"><i class="fa fa-trash"></i> Eliminar</button></td>
+      </tr>
+      <tr>
+        <td scope="row">Segundo examen</td>
+        <td>Prueba002</td>
+        <td>07/05/2024</td>
+        <td>Carlos</td>
+        <td>10</td>
+        <td><button class="btn-delete"><i class="fa fa-trash"></i> Eliminar</button></td>
+      </tr>
+      <tr>
+      <td scope="row">Tercer examen</td>
+        <td>Miercoles</td>
+        <td>15/05/2024</td>
+        <td>Juan</td>
+        <td>6</td>
+        <td><button class="btn-delete"><i class="fa fa-trash"></i> Eliminar</button></td>
+      </tr>
+    </tbody>
+  </table>
+  </div>
+<?php @include '../../components/footer.php'?>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
