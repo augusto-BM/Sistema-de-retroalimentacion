3455dd875b9eb89139c0e957a0089dc774278e07
MatiasSalazar1029 <matiassalazar1029@gmail.com>
2024-06-11 22:26:30 +0200

    avance de rol general

---
+++ vista/dashboard/principal/php-principal/sidebar.php
@@ -0,0 +1,119 @@
+<div class="menu">
+    <ion-icon name="menu-outline"></ion-icon>
+    <ion-icon name="close-outline"></ion-icon>
+</div>
+
+<div class="barra-lateral">
+    <div>
+        <div class="nombre-pagina">
+            <ion-icon id="cloud" name="cloud-outline"></ion-icon>
+            <span>GENERAL</span>
+        </div>
+        <!-- <button class="boton">
+                <ion-icon name="add-outline"></ion-icon>
+                <span>Create new</span>
+            </button> -->
+    </div>
+    <div class="linea mb-3"></div>
+
+    <nav class="navegacion">
+        <div class="seccion">
+            <p><sub>Menu Principal</sub></p>
+            <ul>
+                <li>
+                    <a id="inbox" href="#">
+                        <ion-icon name="mail-unread-outline"></ion-icon>
+                        <span>Inicio</span>
+                    </a>
+                </li>
+                <li>
+                    <div class="dropdown">
+                        
+                        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                            Datos Principales
+                        </button>
+                        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                            <li><a class="dropdown-item" href="#">Clase</a></li>
+                            <li><a class="dropdown-item" href="#">Curso action</a></li>
+                            <li><a class="dropdown-item" href="#">Backoffice</a></li>
+                            <li><a class="dropdown-item" href="#">Supervisor</a></li>
+                            
+                        </ul>
+                    </div>
+                </li>
+                <li>
+                    <a href="#">
+                        <ion-icon name="paper-plane-outline"></ion-icon>
+                        <span>Relacion</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <ion-icon name="star-outline"></ion-icon>
+                        <span>Banco de preguntas</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div class="seccion">
+            <p><sub>Reportes</sub></p>
+            <ul>
+                <li>
+                    <a href="#">
+                        <ion-icon name="document-text-outline"></ion-icon>
+                        <span>Resultado de examenes</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div class="seccion">
+            <p><sub>Administrador</sub></p>
+            <ul>
+                <li>
+                    <a href="#">
+                        <ion-icon name="mail-unread-outline"></ion-icon>
+                        <span>Administrador de usuarios</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <ion-icon name="star-outline"></ion-icon>
+                        <span>Configuracion</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+
+
+    <div>
+        <div class="linea"></div>
+
+        <div class="modo-oscuro">
+            <div class="info">
+                <ion-icon name="moon-outline"></ion-icon>
+                <span>Modo Dark</span>
+            </div>
+            <div class="switch">
+                <div class="base">
+                    <div class="circulo">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="usuario">
+            <img src="/Jhampier.jpg" alt="">
+            <div class="info-usuario">
+                <div class="nombre-email">
+                    <span class="nombre">General</span>
+                    <span class="email">general@gmail.com</span>
+                </div>
+                <ion-icon name="ellipsis-vertical-outline"></ion-icon>
+            </div>
+        </div>
+    </div>
+
+</div>
